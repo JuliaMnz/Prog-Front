68e86a550c3ccce5dfee5bbcf1657bb43701d00a
Júlia Muniz <julia.mco0512@gmail.com>
2025-08-15 00:34:05 +0000

    componentes

--- ex01-next/app/page.js
+++ ex01-next/app/page.js
@@ -1,6 +1,14 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 
+import { Profile } from "@/components/Profile";
+
 export default function Home() {
-  return <div>Olá Turma!</div>;
+  return (
+    <div>
+      <h1>Amazing scientists</h1>
+      <Profile />
+      <Profile />
+    </div>
+  );
 }
