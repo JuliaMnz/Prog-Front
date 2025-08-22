19f4cea2d533d977451fa28f0fa74ff00b5640f8
Júlia Muniz <julia.mco0512@gmail.com>
2025-08-22 03:42:36 +0000

    estilizando

--- atv01-next/atv01-next/app/page.js
+++ atv01-next/atv01-next/app/page.js
@@ -3,9 +3,10 @@ import { Profile } from "@/components/Profile";
 export default function Home() {
   return (
     <div>
-      <h1 className="text-2xl font-bold mb-6">
+      <h1 className="frase">
         Site criado para a disciplina Programação Front-End
       </h1>
+      <hr />
       <Profile />
     </div>
   );
--- atv01-next/atv01-next/components/Profile.js
+++ atv01-next/atv01-next/components/Profile.js
@@ -1,13 +1,13 @@
 export function Profile() {
   return (
-    <div>
-      <img
+    <div className="container-imagem">
+      <img className="foto"
         src="/me.jpg"
         alt="Minha foto"
         width={180}
       />
-      <h2 className="text-xl font-bold">Júlia Muniz</h2>
-      <p className="text-gray-600">
+      <h2 className="meu-nome">Júlia Muniz</h2>
+      <p className="sobre-mim">
         Estudante de Sistemas para Internet, apaixonada por tecnologia, desing e aprendizado contínuo.
         Sempre buscando novos desafios e soluções criativas. ;D
       </p>
