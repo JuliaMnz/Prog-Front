325bdb444d6465f49ee737d3d4960cdb3107d071
Júlia Muniz <julia.mco0512@gmail.com>
2025-08-22 00:29:39 +0000

    continuando atividade

--- atv01-next/atv01-next/app/page.js
+++ atv01-next/atv01-next/app/page.js
@@ -1,8 +1,8 @@
-import Profile from "../components/Profile";
+import { Profile } from "@/components/Profile";
 
 export default function Home() {
   return (
-    <div className="min-h-screen flex flex-col items-center justify-center">
+    <div>
       <h1 className="text-2xl font-bold mb-6">
         Site criado para a disciplina Programação Front-End
       </h1>
--- atv01-next/atv01-next/components/Profile.js
+++ atv01-next/atv01-next/components/Profile.js
@@ -1,10 +1,10 @@
-export default function Profile() {
+export function Profile() {
   return (
-    <div className="p-4 border rounded-lg max-w-sm mx-auto shadow-md text-center">
+    <div>
       <img
         src="/me.jpg"
         alt="Minha foto"
-        className="w-32 h-32 rounded-full mx-auto mb-4"
+        width={180}
       />
       <h2 className="text-xl font-bold">Júlia Muniz</h2>
       <p className="text-gray-600">
